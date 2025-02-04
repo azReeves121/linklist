2006444b359160c112340a8e934b835202b7efee
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2025-02-04 01:02:08 -0330

    main

--- UndoRedoManager.java
+++ UndoRedoManager.java
@@ -45,8 +45,18 @@ public class UndoRedoManager<T> {
         currentState = currentState.next;
         return currentState.state;
     }
+
     public static void main(String[] args) {
+        UndoRedoManager<String> manager = new UndoRedoManager<>();
 
+        manager.addState("State 1");
+        manager.addState("State 2");
+        manager.addState("State 3");
 
+        System.out.println("Undo: " + manager.undo());
+        System.out.println("Undo: " + manager.undo());
+        System.out.println("Redo: " + manager.redo());
+        System.out.println("Redo: " + manager.redo());
+        System.out.println("Redo: " + manager.redo());
     }
 }
